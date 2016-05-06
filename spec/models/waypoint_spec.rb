119dc6881a617fd36f340ae35c96504a698b0edc
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-06 19:53:22 +0100

    added validation for lat long

--- spec/models/waypoint_spec.rb
+++ spec/models/waypoint_spec.rb
@@ -4,4 +4,14 @@ describe Waypoint, type: :model do
 
   it { is_expected.to belong_to :route }
 
+  it 'validates the latitude' do
+    invalid_waypoint = Waypoint.new(latitude: 100.0, longitude: 10.0)
+    expect(invalid_waypoint.save).to be false
+  end
+
+  it 'validates the longitude' do
+    invalid_waypoint = Waypoint.new(latitude: 10.0, longitude: 200.0)
+    expect(invalid_waypoint.save).to be false
+  end
+
 end
