d008a25a194ccd8a8b361a1d1bcf67cf10a741a3
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-09 10:45:47 +0100

    generated new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506195044) do
+ActiveRecord::Schema.define(version: 20160509094523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "journeys", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "waypoints", force: :cascade do |t|
